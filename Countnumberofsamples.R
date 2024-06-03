a3e3f6f1c94edef6dd99b7a2054a1a8c1b7abdf2
kimbaonguyen <baokimnguyen2254@gmail.com>
2024-06-03 13:32:44 +0700

    Add files via upload

---
+++ Countnumberofsamples.R
@@ -0,0 +1,4 @@
+sum(bladder_cleaned_meta_v2_2846[["turbt_cystectomy_JD"]] == "TURBT", na.rm = TRUE)
+sum(bladder_cleaned_meta_v2_2846[["turbt_cystectomy_JD"]] == "TURBT", na.rm = TRUE) + sum(bladder_cleaned_meta_v2_2846[["sample_storage_JD"]] == "FFPE", na.rm = TRUE)
+sum(bladder_cleaned_meta_v2_2846[["sample_storage_JD"]] == "FFPE", na.rm = TRUE)
+                                                                            
